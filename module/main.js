b79ea750260b221c8558a31eba5c14d7909b7e8d
HUGO LOPES DE SOUZA <24122289@dsn127m1247443.cfp127aluno.local>
2025-08-07 11:00:25 -0300

    first commit

--- module/main.js
+++ module/main.js
@@ -4,12 +4,12 @@ import { getPhotos } from "./app.js";
 
 async function  createImages() {
     const photosData = await getPhotos()
-    const containerImg = document.querySelector('.container_images')
+    const containerImg = document.querySelector('.slides')
    
     photosData.forEach(element => {
         const divIMG = document.createElement('img')
         divIMG.src = element.imagem
-        
+        divIMG.classList.add('slide')
 
         containerImg.appendChild(divIMG)
     })
